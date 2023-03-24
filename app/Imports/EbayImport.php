287667e29915bd894a54bbd51f277ef3958c8518
GreatGleb <sugak.gleb@mail.ru>
2023-03-24 15:40:14 +0200

    add class for read Excel files

---
+++ app/Imports/EbayImport.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Imports;
+
+use Illuminate\Support\Collection;
+use Maatwebsite\Excel\Concerns\Importable;
+use Maatwebsite\Excel\Concerns\ToCollection;
+
+class EbayImport implements ToCollection
+{
+    use Importable;
+
+    /**
+     * @param Collection $collection
+     */
+    public function collection(Collection $collection)
+    {
+        //
+    }
+}
